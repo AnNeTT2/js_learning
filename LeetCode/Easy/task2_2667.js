e059ccf723a4dadb8cff882ad89997f3f1d4b756
AnNeTT2 <anka7220@gmail.com>
2023-08-11 18:46:23 +0300

    https://github.com/AnNeTT2 leetcode  - Create Hello World Function

---
+++ LeetCode/Easy/task2_2667.js
@@ -0,0 +1,13 @@
+/**
+ * @return {Function}
+ */
+let createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
