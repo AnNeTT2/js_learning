929dcd8e2e5c609c2d52cc4a85e44c9573e9c336
AnNeTT2 <anka7220@gmail.com>
2023-09-02 11:16:31 +0300

    Hexlet  -  a function that flips a number

---
+++ Hexlet/reverseInt.js
@@ -0,0 +1,10 @@
+ // Реализуйте и экспортируйте по умолчанию функцию, которая переворачивает цифры в переданном числе и возвращает новое число.
+
+export default function reverseInt(num) {
+      let number = 133;
+     return number.toString().split("").reverse().join("");
+
+}
+console.log(reverseInt());
+
+
